631cc02909ab41634d68737347d75db6219c5d0c
leebom <d0201d@hanmail.net>
2020-10-15 18:30:27 +0900

    이미지 js로 다운로드 기능 추가
    이미지 js로 다운로드 기능 추가

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2"; 
 const CANVAS_SIZE = 700;
@@ -10,6 +11,8 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = "#fff";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -62,6 +65,17 @@ function HandleCanvasClick() {
     }
 }
 
+function  handleCM(event) {
+    event.preventDefault();
+}
+function handleSaveClick() {
+    const image = canvas.toDataURL();
+   const link = document.createElement("a");
+   link.href = image;
+   link.download = "PaintJS [EXPORT]";
+
+    link.click();
+}
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
@@ -69,6 +83,7 @@ if (canvas) {
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click", HandleCanvasClick);
+    addEventListener("contextmenu", handleCM);   //우클릭 방지
 }
 
 Array.from(colors).forEach(color =>
@@ -81,4 +96,8 @@ if(range) {
 
 if(mode) {
     mode.addEventListener("click", handleModeClick)
+}
+
+if(saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick);
 }
